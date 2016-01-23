ffd806f25029b09f9b84c7b21c854bd9c98b50a8
yudylaw <20liuyu@163.com>
2016-01-23 11:25:54 +0800

    fixed css issue.

---
+++ install/ts_area.sql
@@ -0,0 +1,5 @@
+DELETE from `ts_area`;
+INSERT INTO `ts_area` VALUES (3308, '霍邱县参训单位', 0, 2);
+INSERT INTO `ts_area` VALUES (3309, '霍邱县城关附小', 3308, 1);
+INSERT INTO `ts_area` VALUES (3310, '石店中学', 3308, 2);
+INSERT INTO `ts_area` VALUES (3311, '叶集初中', 3308, 3);
---
+++ install/ts_user_group.sql
@@ -0,0 +1,9 @@
+DELETE FROM `ts_user_group`;
+INSERT INTO `ts_user_group` VALUES (1, '超级管理员', 1453516303, 'v_05.png', 0, 'public', 0);
+INSERT INTO `ts_user_group` VALUES (2, '巡逻员', 1430293047, '-1', 0, 'public', 0);
+INSERT INTO `ts_user_group` VALUES (3, '教师', 1453516460, '-1', 0, 'public', 0);
+INSERT INTO `ts_user_group` VALUES (4, '禁言用户', 1430985083, 'v_04.png', 0, 'public', 0);
+INSERT INTO `ts_user_group` VALUES (5, '个人认证', 1430383414, 'v_01.png', 0, 'public', 1);
+INSERT INTO `ts_user_group` VALUES (6, '机构认证', 1430383423, 'v_03.png', 0, 'public', 1);
+INSERT INTO `ts_user_group` VALUES (8, '班级管理员', 1453516275, '-1', 0, 'public', 0);
+INSERT INTO `ts_user_group` VALUES (9, '区域管理员', 1453516288, '-1', 0, 'public', 0);
