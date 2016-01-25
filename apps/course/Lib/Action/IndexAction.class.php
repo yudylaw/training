0665b982252ac56eb9a415d1f9b6ebab0c28dedf
s9zhao <javajava.happy@163.com>
2016-01-25 17:19:15 +0800

    修改课程action

--- apps/course/Lib/Action/IndexAction.class.php
+++ apps/course/Lib/Action/IndexAction.class.php
@@ -1,8 +1,16 @@
 <?php
-
+/**
+ * 课程学习
+ * @author sjzhao
+ *
+ */
 class IndexAction extends Action {
     
     public function index() {
+        $this->display();
+    }
+    
+    public function detail() {
         $this->display();
     }
 }
