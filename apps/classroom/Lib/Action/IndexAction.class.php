91f1455763c59c6e5f719cdf63656af64e462ba6
yudylaw <20liuyu@163.com>
2016-03-09 23:04:42 +0800

    add members

--- apps/classroom/Lib/Action/IndexAction.class.php
+++ apps/classroom/Lib/Action/IndexAction.class.php
@@ -16,11 +16,28 @@ class IndexAction extends Action {
         $ids = implode(',', $classroomIds);
         
         $sql = "SELECT w.weiba_name, w.weiba_id, w.ctime, w.`subject`, w.follower_count, u.uname ";
-        $sql .="FROM ts_weiba w LEFT JOIN ts_user u ON w.admin_uid = u.uid AND w.is_del = 0 AND weiba_id IN (".$ids.")";
+        $sql .="FROM ts_weiba w LEFT JOIN ts_user u ON w.admin_uid = u.uid WHERE w.is_del = 0 AND weiba_id IN (".$ids.")";
         
         $classroomList = M('weiba')->query($sql);
         
         $this->assign('classroomList', $classroomList);
+        $this->display();
+    }
+    
+    public function member_list() {
+        $class_id = intval($_REQUEST['class_id']);
+        $classroom = M('weiba')->where(array('weiba_id'=>$class_id, 'is_del'=>0))->find();
+    
+        if(empty($classroom)) {
+            $this->error("班级不存在");
+        }
+    
+        $sql = "SELECT wf.*, u.uname, u.location, u.phone, u.sex from ts_weiba_follow wf LEFT JOIN ts_user u ON wf.follower_uid = u.uid";
+        $sql .=" WHERE wf.weiba_id = ".$class_id;
+        $members = M('weiba_follow')->query($sql);
+    
+        $this->assign('classroom', $classroom);
+        $this->assign('members', $members);
         $this->display();
     }
     
