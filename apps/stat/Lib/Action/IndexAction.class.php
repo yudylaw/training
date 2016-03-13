e3e724ca7a7e1ed0eddac349a91e9ab212314c9f
yudylaw <20liuyu@163.com>
2016-03-13 23:03:18 +0800

    add 考试作业

--- apps/stat/Lib/Action/IndexAction.class.php
+++ apps/stat/Lib/Action/IndexAction.class.php
@@ -34,22 +34,76 @@ class IndexAction extends Action {
             $sql .=" FROM ts_course_learning cl LEFT JOIN ts_user u ON cl.uid = u.uid WHERE cl.course_id=".$id;
             
             $records = M('course')->query($sql);
-            $this->assign('courses', $courses);
+            $this->assign('records', $records);
         }
         
-
+        $this->assign('courses', $courses);
         $this->assign('course_id', $id);
-        $this->assign('records', $records);
+        
         $this->display();
     }
     
     //作业统计
     public function homework() {
+        $id = intval($_REQUEST['id']);
+        
+        if ($id > 0) {
+            $homework = M('homework')->where(array('id'=>$id, 'type'=>1, 'is_del'=>0))->find();
+        
+            if (empty($homework)) {
+                $this->error("作业不存在");
+            }
+        }
+        
+        $sql = "select id,name from ts_homework where type = 1 and is_del = 0 order by id desc limit 0,10";//最近10个作业
+        $homeworks = M('homework')->query($sql);
+        
+        if ($id < 1 && !empty($homeworks)) {
+            $id = $homeworks[0]['id'];//取第一个
+        }
+        
+        if ($id > 0) {
+            $sql = "SELECT u.uname, u.location,u.phone,hr.score,hr.is_grade,hr.ctime from ts_homework_record hr";
+            $sql .=" LEFT JOIN ts_user u ON hr.uid = u.uid WHERE hr.hw_id=".$id;
+        
+            $records = M('homework')->query($sql);
+            $this->assign('records', $records);
+        }
+        
+        $this->assign('homeworks', $homeworks);
+        $this->assign('hw_id', $id);
         $this->display();
     }
     
     //考试统计
     public function test() {
+        $id = intval($_REQUEST['id']);
+        
+        if ($id > 0) {
+            $homework = M('homework')->where(array('id'=>$id, 'type'=>0, 'is_del'=>0))->find();
+        
+            if (empty($homework)) {
+                $this->error("考试不存在");
+            }
+        }
+        
+        $sql = "select id,name from ts_homework where type = 0 and is_del = 0 order by id desc limit 0,10";//最近10个作业
+        $homeworks = M('homework')->query($sql);
+        
+        if ($id < 1 && !empty($homeworks)) {
+            $id = $homeworks[0]['id'];//取第一个
+        }
+        
+        if ($id > 0) {
+            $sql = "SELECT u.uname, u.location,u.phone,hr.score,hr.is_grade,hr.ctime from ts_homework_record hr";
+            $sql .=" LEFT JOIN ts_user u ON hr.uid = u.uid WHERE hr.hw_id=".$id;
+        
+            $records = M('homework')->query($sql);
+            $this->assign('records', $records);
+        }
+        
+        $this->assign('homeworks', $homeworks);
+        $this->assign('hw_id', $id);
         $this->display();
     }
     
